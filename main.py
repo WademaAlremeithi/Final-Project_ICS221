51936fcd006f5ec71f74bdb6f31692119aa78a5b
khadejaAref <160011597+khadejaAref@users.noreply.github.com>
2024-04-23 11:59:47 +0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,6 @@
+class Post:
+    def __init__(self, datetime, post, post_owner, views):
+        self.datetime = datetime
+        self.post = post
+        self.post_owner = post_owner
+        self.views =views

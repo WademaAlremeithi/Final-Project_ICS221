5907579dd3aaa07afce44db759eda9ca875df580
WademaAlremeithi <160010601+WademaAlremeithi@users.noreply.github.com>
2024-04-26 15:57:55 +0400

    Update partB.py

--- partB.py
+++ partB.py
@@ -1,4 +1,5 @@
 #PART B
+#Represent the road network using a graph
 import networkx as nx
 import matplotlib.pyplot as plt
 class RoadNetwork:
@@ -17,3 +18,39 @@ class RoadNetwork:
         edge_labels = {(u, v): f"{d['road_name']}\nWeight: {d['weight']}" for u, v, d in self.graph.edges(data=True)}
         nx.draw_networkx_edge_labels(self.graph, pos, edge_labels=edge_labels)
         plt.show()
+
+#Point A to Point B in the shortest distance possible
+#Using Dijkstra’s algorithm
+def dijkstra(graph, start, end):
+    #Create empty dictionaries for the distances and paths
+    distances = {node: float('infinity') for node in graph}
+    paths = {node: [] for node in graph}
+
+    #Initialize the distance and path for start vertex
+    distances[start] = 0
+    paths[start] = [start]
+    #create a set to keep track of visted vertices
+    visited = set()
+
+    #iterate until we reach end point(Point B)
+    while end not in visited:
+        shortest_dist = float('infinity')
+        here = None
+        for v in graph:
+            if v not in visited and distances[v] < shortest_dist:
+                shortest_dist = distances[v]
+                here = v
+        #Add the current vertex to visited 
+        visited.add(here)
+        #Update distances and paths for neighbors of the current vertex
+        for neighbor in graph.neighbors(here):
+            if neighbor not in visited:
+                new_distance = distances[here] + graph[here][neighbor]['weight']
+                if new_distance < distances[neighbor]:
+                    distances[neighbor] = new_distance
+                    paths[neighbor] = paths[here] + [neighbor]
+                
+    return distances[end], paths[end]
+
+    
+                            
